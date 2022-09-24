ec80964c8755ca9eb3c08c9cf7af17431ae497c7
Ameen <ameendv00@gmail.com>
2022-09-24 11:55:48 +0530

    Routes setup

---
+++ frontend/src/components/user/Videos/Videos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Videos() {
+  return (
+    <div>
+      <h1>Vides will be listed here</h1>
+    </div>
+  )
+}
+
+export default Videos
---
+++ frontend/src/pages/user/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Videos from '../../components/user/Videos/Videos'
+
+function Home() {
+  return (
+    <div>
+      <Videos />
+    </div>
+  )
+}
+
+export default Home
